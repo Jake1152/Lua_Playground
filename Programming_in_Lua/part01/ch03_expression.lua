e1b6f5ec63326a2163c6a43414d381515ab64a8b
Jake1152 <rancho974@gmail.com>
2025-01-26 19:41:06 +0900

    feat: 3.5 length operator

--- Programming_in_Lua/part01/ch03_expression.lua
+++ Programming_in_Lua/part01/ch03_expression.lua
@@ -22,11 +22,76 @@
 -- print("\n\n")
 
 -- # 3.4 concat
-print('"Hello " .. "World" : ', "Hello " .. "World")
-print("0 .. 1 : ", 0 .. 1)
-print("000 .. 01 : ", 000 .. 01)
+-- print('"Hello " .. "World" : ', "Hello " .. "World")
+-- print("0 .. 1 : ", 0 .. 1)
+-- print("000 .. 01 : ", 000 .. 01)
 
-a = "Hello"
-print('a .. " World" : ', a .. " World")
-print("a : ", a)
+-- a = "Hello"
+-- print('a .. " World" : ', a .. " World")
+-- print("a : ", a)
 
+
+-- # 3.5 length operator
+--[[
+길이연산자는 문자열과 테이블에 쓰임
+문자열에 쓰이는 경우 문자열이 몇 바이트인지, 반환한다.
+테이블에 쓰는 경우에는 순열의 길이를 반환한다.
+--]]
+print_table = function(tbl)
+    for i, v in ipairs(tbl) do
+      print("v : ", v)
+    end
+  end
+
+a = {
+    1, 2, 3, 4, 5
+}
+print_table(a)
+print("a[#a] : ", a[#a])
+a[#a] = nil
+v = 42
+a[#a + 1] = v
+
+print_table(a)
+
+
+--[[
+루아에서 리스트가 실제로는 테이블이다
+리스트에 nil로 비어있는 경우 예상하지 못한 결과를 낸다.
+리스트로 정의된 순열에만 유효하다.
+순열은 연속된 숫자 키 집합으로 구성된 테이블이다. (값이 nil인 키는 실제로 테이블에 없는 것이다.)
+
+특히, 숫자로된 키가 없는 테이블은 길이가 0인 순열이다.
+--]]
+
+a = {}
+a[1] = 1
+a[2] = nil
+a[3] = 1
+a[4] = 1
+print("#a : ", #a)
+
+a = {}
+a[1] = 1
+a[10000] = 1
+print("#a : ", #a)
+
+a = {}
+a[1] = 1
+a[10000] = nil
+print("#a : ", #a)
+
+
+a = {10, 20, 30, nil, nil}
+print("#a : ", #a)
+
+
+a = {10, 20, 30, nil, nil, 60}
+print("#a : ", #a)
+
+
+a = {10, 20, 30, nil, nil, 60, nil}
+print("#a : ", #a)
+
+a = {10, 20, 30, nil, nil, 60, nil, 80}
+print("#a : ", #a)
